458750614df8398bb1f946f8d4180ec85b54d228
tropylium <adg4096@gmail.com>
2023-07-14 22:53:24 -0700

    eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
+    'plugin:jsdoc/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -23,5 +24,59 @@ module.exports = {
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
+    'no-tabs': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      // allow UPPER_CASE enum members:
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE', 'camelCase', 'PascalCase']
+      },
+      // ... and the rest is just restating the naming-convention defaults (which is necessary,
+      // because overriding this option erases the default naming convention rules and replaces them with these rules)
+      {
+        selector: 'default',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+
+    '@typescript-eslint/explicit-function-return-type': [ 'error', { allowExpressions: true } ],
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/no-inferrable-types': 'warn',
+    '@typescript-eslint/no-for-in-array': 'error',
+    '@typescript-eslint/prefer-for-of': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+    '@typescript-eslint/prefer-readonly': 'error',
+    '@typescript-eslint/strict-boolean-expressions': 'error',
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+
+    'jsdoc/no-types': 'error', // forbid types in @param, because they're redundant with TS declaration
+    'jsdoc/require-param-type': 'off', // ditto
+    'jsdoc/require-returns-type': 'off', // ditto
+    'jsdoc/tag-lines': 'off', // too picky about spacing
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+/**
+ * The base component of this application.
+ *
+ * @returns the component.
+ */
+function App(): React.ReactElement {
   const [count, setCount] = useState(0)
 
   return (
